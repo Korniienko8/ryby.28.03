ba99804d4d2b01188685ef6035b0cfd1c28dda72
Korniienko8 <nastyakornienko8@gmail.com>
2025-03-28 16:47:41 +0100

    Add files via upload

---
+++ wedkuj.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkowanie</title>
+</head>
+<style>
+
+</style>
+
+<body>
+    <div class="baner">
+        <h1>Portal dla wędkarzy</h1>
+    </div>
+    <div class="lewy">
+        <h3>Ryby zamieszkujące rzeki</h3>
+        <ol>
+            <?php  ?>
+        </ol>
+    </div>
+    <div class="lewy">
+        <h3>Ryby drapieżne naszych wód</h3>
+    
+    <table>
+        <?php ?>
+        <thead>
+            <tr>
+                <th>L.p</th>
+                <th>Gatunek</th>
+                <th>Występowanie</th>
+            </tr>
+        </thead>
+
+    </table>
+        </div>
+        <div class="prawy">
+            <img src="ryba1.jpg" alt="Sum">
+            <a href="kwerendy.txt">Pobierz kwerendy</a>
+        </div>
+
+   
+
+</body>
+
+</html>
